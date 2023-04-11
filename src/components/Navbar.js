150035a306e4bfe860ebf186ebb95af77e1b2fbf
Tej7666 <tejas.satpute.98@gmail.com>
2023-04-11 22:55:53 +0530

    added gradient colours.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div>
-      <nav className="bg-gradient-to-r from-slate-900 to-slate-700 py-4">
+      <nav className="bg-gradient-to-r from-slate-800 to-slate-600 py-4">
         <h1 className="text-3xl font-bold text-center text-white">Top Courses</h1>
       </nav>
     </div>
